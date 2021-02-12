01934ad150f83ab963ddcd8c78c7f7b50263f332
Mufaddal Shakir <muffi606@gmail.com>
2021-02-12 17:56:52 +0500

    Add files via upload

---
+++ backend/models/userModels.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+	name: {type: String, required: true},
+	email:{type: String, required: true},
+	password:{type: String, required: true},
+	isAdmin: {type: Boolean, required, default: false}
+    });
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
