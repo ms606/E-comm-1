9c4bdb7d739a6bccf008617dc7c0d83394c4134c
Mufaddal Shakir <muffi606@gmail.com>
2021-02-14 22:24:37 +0500

    Add files via upload

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,26 @@
 import express from 'express';
-import user from '../models/userModels';
+import User from '../models/userModel';
+import { getToken } from '../util';
 
 const router = express.Router();
+
+router.post('/signin', async (req, res) => {
+	const signinUser = await User.findOne({
+		email: req.body.email,
+		password: req.body.password
+	});
+	if (signinUser){
+		res.send({
+		 _id: signinUser.id,
+		 name: signinUser.name,
+		 email: signinUser.email,
+		 isAdmin: signinUser.isAdmin,
+		 token: getToken(user)	
+		})
+	} else {
+		res.status(401).send({msg: 'Invalid Email or Password.'});
+	}
+})
 
 router.get("/createadmin", async (req, res) => {
 	try {
